b9d3ed4fa4e622389928ee00ca9ee57a25d619c0
David Zuckerman <davidz@wix.com>
2014-03-30 15:08:48 +0300

    updating libraries, preparing for publish to npm

---
+++ samples/examples.js
@@ -0,0 +1,42 @@
+/**
+ * This example shows how to use the Wix REST API with node
+ * Replace [YOUR SECRET KEY], [YOUR APP ID] and [YOUR INSTANCE ID] with the appropriate values to
+ * use the first example.
+ *
+ * To post an Activity,  please also fill in the [USER SESSION] value
+ *
+ * More information about the Wix RESTful API can be found here:
+ * http://dev.wix.com/docs/display/DRAF/Working+with+the+Wix+RESTful+API
+ *
+ */
+
+
+var wixapi = require('./../lib/WixClient.js');
+
+var wix = wixapi.getAPI("[YOUR SECRET KEY]", "[YOUR APP ID]", "[YOUR INSTANCE ID]");
+
+
+wix.Activities.getTypes().then(function(data) {
+    console.log(data);
+}, function(error) {
+    console.log(error);
+});
+
+var activity = wix.Activities.newActivity(wixapi.ACTIVITY_TYPES.CONTACT_FORM);
+activity.contactUpdate.addEmail("main", "name@example.com").withName("Your", "Customer");
+activity.withLocationUrl("test.com").withActivityDetails("This is a test activity post", "http://www.example.com");
+activity.activityInfo.addField("name", "Your Customer").addField("email", "name@example.com");
+
+wix.Activities.postActivity(activity,
+    "[USER SESSION]")
+    .then(function(data) {
+        console.log(data);
+    }, function(error) {
+        console.log(error);
+    });
+
+wix.Insights.getActivitiesSummary().then(function(data) {
+    console.log(data);
+}, function(error) {
+    console.log(error);
+});
