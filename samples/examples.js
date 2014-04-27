a3672820e974bb9559cd82962e6b49b658026937
David Zuckerman <davidz@wix.com>
2014-04-27 17:44:01 +0300

    fix example code

--- samples/examples.js
+++ samples/examples.js
@@ -20,17 +20,25 @@ wix.Activities.getTypes().then(function(data) {
     console.log(error);
 });
 
-var activity = wix.Activities.newActivity(wixapi.ACTIVITY_TYPES.CONTACT_FORM);
-activity.contactUpdate.addEmail("main", "name@example.com").withName("Your", "Customer");
-activity.withLocationUrl("test.com").withActivityDetails("This is a test activity post", "http://www.example.com");
-activity.activityInfo.addField("name", "Your Customer").addField("email", "name@example.com");
+var activity = wix.Activities.newActivity(wix.Activities.TYPES.CONTACT_FORM);
+var cu = activity.contactUpdate;
+cu.addEmail(cu.newEmail().withTag("main").withEmail("name@wexample.com"));
+
+cu.name.withFirst("Your").withLast("Customer");
+
+activity.withLocationUrl("test.com").withActivityDetails("This is a test activity post", "http://www.test.com");
+var ai = activity.activityInfo;
+ai.addField(ai.newField().withName("email").withValue("name@wexample.com"));
+ai.addField(ai.newField().withName("first").withValue("Your"));
+ai.addField(ai.newField().withName("last").withValue("Customer"));
+
 
 wix.Activities.postActivity(activity,
     "[USER SESSION]")
     .then(function(data) {
-        console.log(data);
+        console.log("Success! " + data);
     }, function(error) {
-        console.log(error);
+        console.log("Oh no! " + error);
     });
 
 wix.Insights.getActivitiesSummary().then(function(data) {
