09b24d4e48f82dbc48066360d5ddb29f79b8634f
Karen Cohen <karenc@wix.com>
2014-10-27 17:03:09 +0200

    /sites/site

---
+++ test/sites.js
@@ -0,0 +1,32 @@
+/**
+* Created by Karen_Cohen on 7/16/14.
+*/
+
+var should = require('should');
+var request = require('supertest');
+var expect = require('expect.js');
+var config = require("./config.js");
+var APP_SECRET = config.appSecret;
+var APP_KEY = config.appKey;
+var INSTANCE_ID = config.instanceId;
+
+var wixLib = require( '../lib/WixClient.js' );
+var api = wixLib.getAPI(APP_SECRET,APP_KEY, INSTANCE_ID);
+
+describe('Sites', function() {
+    this.timeout(10000);
+
+    describe('getSite', function() {
+        it('should return return Site address', function(done) {
+
+            api.Sites.getSite()
+                .then(function(data) {
+                    data.should.not.equal(undefined);
+                    console.log(data);
+                    done();
+                }, function(error) {
+                    done(error);
+                }).done(null, done);
+        });
+    });
+});
