2b0ee05a9ca897d8c414205f03862694bf5442e3
Karen Cohen <karenc@wix.com>
2014-12-08 15:20:30 +0200

    new Ecomm Activities

--- test/config.js
+++ test/config.js
@@ -1,9 +1,9 @@
-var config = {
-    "appSecret" : "APP_SECRET", // <---------- REPLACE THIS WITH YOUR OWN APP SECRET KEY
-    "appKey" : "APP_KEY", // <---------- REPLACE THIS WITH YOUR OWN APP KEY
-    "instance" : "INSTANCE",
-    "instanceId" : "INSTANCE+ID",
-    "sessionId" : "SESSION_ID"
+var config = { // page and widget vine app
+    "appSecret" : "7f00e181-fcf7-4058-a116-88607c49049e",
+    "appKey" : "137385b2-a44a-72c6-ef0a-b4ac42484821",
+    "instance" : "tps2E8I9RszwPpav6MnxSM6DwhDoaw_lnupHJG1Nat0.eyJpbnN0YW5jZUlkIjoiMTM5NTdjMDYtM2E2OS1kYjI0LWVkYWEtNjk1MmQ4NDkzZDc2Iiwic2lnbkRhdGUiOiIyMDE0LTA5LTE5VDIzOjQ3OjMzLjgzNloiLCJ1aWQiOiJhZDA4ZWM2ZS0zMzk1LTRkYzgtOWJhMC1mMDVlZWQ4YTYxOGUiLCJwZXJtaXNzaW9ucyI6Ik9XTkVSIiwiaXBBbmRQb3J0IjoiMTYyLjIxNy43NS4zNC80NzMxOCIsInZlbmRvclByb2R1Y3RJZCI6bnVsbCwiZGVtb01vZGUiOmZhbHNlfQ",
+    "instanceId" : "1373871e-889f-44f7-0e15-f3d8b72c21cc",
+    "sessionId" : "02594992c9c57f61148351a766cf2ab79f7a7007ce309a16fc2b6475b0895b5b09250b55ec2c4cdba152aef47daded4d1e60994d53964e647acf431e4f798bcd0b93ce826ad6aa27a9c95ffedb05f421b7b1419780cf6036d4fd8efd847f9877"
 };
 
 module.exports = config;
