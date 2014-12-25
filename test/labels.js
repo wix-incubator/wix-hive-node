3ab256c9cd9cf1d32b12e51e53f60e6bde5fd058
Karen Cohen <karenc@wix.com>
2014-12-25 11:01:42 +0200

    Labels support

---
+++ test/labels.js
@@ -0,0 +1,75 @@
+/**
+* Created by Karen_Cohen on 8/25/14.
+*/
+var should = require('should');
+var assert = require('assert');
+var expect = require('expect.js');
+var config = require("./config.js");
+var APP_SECRET = config.appSecret;
+var APP_KEY = config.appKey;
+var INSTANCE_ID = config.instanceId;
+
+describe('Labels', function() {
+
+    var wixLib = require( '../lib/WixClient.js' );
+    var api = wixLib.getAPI(APP_SECRET,APP_KEY, INSTANCE_ID);
+
+    describe('getLabels', function() {
+        this.timeout(10000);
+        it('should not throw error when called with no parameters', function (done) {
+
+            api.Labels.getLabels().then(
+                function(pagingLabelsResult) {
+                    pagingLabelsResult.should.not.equal(undefined);
+                    pagingLabelsResult.should.be.a.Object;
+                    pagingLabelsResult.should.not.be.empty;
+                    should.exist(pagingLabelsResult.currentData.pageSize);
+                    done();
+                },
+                function(error) {
+                    done(error);
+                }
+            ).done(null, done);
+        });
+        it('should return a non-empty list of Labels when called with no parameters', function (done) {
+
+            api.Labels.getLabels().then(
+                function(pagingLabelsResult) {
+                    pagingLabelsResult.should.not.equal(undefined);
+                    pagingLabelsResult.should.be.a.Object;
+                    pagingLabelsResult.should.not.be.empty;
+                    pagingLabelsResult.currentData.results.should.be.a.Array;
+                    pagingLabelsResult.currentData.results.should.not.have.length(0);
+                    should.exist(pagingLabelsResult.currentData.pageSize);
+                    done();
+                },
+                function(error) {
+                    done(error);
+                }
+            ).done(null, done);
+        });
+        it('should return a non-empty list of Labels when called with pageSize parameter', function (done) {
+
+            api.Labels.getLabels(null,
+                {
+                    pageSize: 50
+                }
+            ).then(
+                function(pagingLabelsResult) {
+                    pagingLabelsResult.should.not.equal(undefined);
+                    pagingLabelsResult.should.be.a.Object;
+                    pagingLabelsResult.should.not.be.empty;
+                    pagingLabelsResult.currentData.results.should.be.a.Array;
+                    pagingLabelsResult.currentData.results.should.not.have.length(0);
+                    pagingLabelsResult.currentData.total.should.not.be.eql(0);
+                    should.exist(pagingLabelsResult.currentData.pageSize);
+                    assert(pagingLabelsResult.currentData.pageSize <= 50);
+                    done();
+                },
+                function(error) {
+                    done(error);
+                }
+            ).done(null, done);
+        });
+    });
+});
