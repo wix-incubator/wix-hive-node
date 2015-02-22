bb34872611a29c0b0b129c922704b98604767f6b
Karen Cohen <karenc@wix.com>
2015-02-22 20:07:10 +0200

    .

--- test/config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    "appSecret" : "APP_SECRET", // <---------- REPLACE THIS WITH YOUR OWN APP SECRET KEY
-    "appKey" : "APP_KEY", // <---------- REPLACE THIS WITH YOUR OWN APP KEY
-    "instance" : "INSTANCE",
-    "instanceId" : "INSTANCE+ID",
-    "sessionId" : "SESSION_ID"
-};
-
-module.exports = config;
