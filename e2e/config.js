7d1d2ce77d5e7d478f13242067f3de872921254f
Karen Cohen <karenc@wix.com>
2015-03-10 12:51:17 +0200

    Revert "."
    This reverts commit 592251b4133eebd85472d9029ec733df1bbb2c8b.

---
+++ e2e/config.js
@@ -0,0 +1,9 @@
+var config = {
+    "appSecret" : "APP_SECRET", // <---------- REPLACE THIS WITH YOUR OWN APP SECRET KEY
+    "appKey" : "APP_KEY", // <---------- REPLACE THIS WITH YOUR OWN APP KEY
+    "instance" : "INSTANCE",
+    "instanceId" : "INSTANCE+ID",
+    "sessionId" : "SESSION_ID"
+};
+
+module.exports = config;
